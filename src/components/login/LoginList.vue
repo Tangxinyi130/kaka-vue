e7c97be02674ee07a5f519d2b1919e3cb957e6c0
TXY <532868890@qq.com>
2018-10-16 14:55:21 +0800

    登录相关

--- src/components/login/LoginList.vue
+++ src/components/login/LoginList.vue
@@ -1,47 +1,74 @@
 <template>
     <div>
       <h3>登录列表</h3>
-      <form action="http://localhost:3000/users/doLogin" method="post">
+      <form action="" method="post">
         手机号：<input type="text" name="username">
         密码：<input type="password" name="password">
-        <input type="submit" value="登录">
+        <input type="button" value="登录">
       </form>
-      <!--<form class="form-horizontal">-->
-        <!--<div class="form-group">-->
-          <!--<label for="inputEmail3" class="col-sm-2 control-label">用户名</label>-->
-          <!--<div class="col-sm-10">-->
-            <!--<input type="email" class="form-control" id="inputEmail3" placeholder="用户名">-->
-          <!--</div>-->
-        <!--</div>-->
-        <!--<div class="form-group">-->
-          <!--<label for="inputPassword3" class="col-sm-2 control-label">密码</label>-->
-          <!--<div class="col-sm-10">-->
-            <!--<input type="password" class="form-control" id="inputPassword3" placeholder="密码">-->
-          <!--</div>-->
-        <!--</div>-->
-        <!--<div class="form-group">-->
-          <!--<div class="col-sm-offset-2 col-sm-10">-->
-            <!--<div class="checkbox">-->
-              <!--<label>-->
-                <!--<input type="checkbox"> 记住密码-->
-              <!--</label>-->
-            <!--</div>-->
-          <!--</div>-->
-        <!--</div>-->
-        <!--<div class="form-group">-->
-          <!--<div class="col-sm-offset-2 col-sm-10">-->
-            <!--<button type="submit" class="btn btn-default"><router-link to="/">登录</router-link></button>-->
-          <!--</div>-->
-        <!--</div>-->
-      <!--</form>-->
-
     </div>
 </template>
 
 <script>
-    export default {
-        name: "LoginList"
+    window.onload = function () {
+          $("input:last").click(function () {
+          let user = {};
+          user.username = $("[name='username']").val();
+          user.password = $("[name='password']").val();
+          $.ajax({
+            // xhrFields: {
+            //   withCredentials: true
+          // },
+          url: "http://localhost:3000/users/doLogin",
+          type: "post",
+          data: {
+            username: user.username,
+            password: user.password
+          },
+          success: function (result) {
+            if (result.data == 1) {
+              alert("用户名错误");
+            } else if (result.data == 2) {
+              alert("密码错误");
+            } else if (result.data == 3) {
+              alert("登录成功");
+
+              save(user.username);
+
+              location.href = "http://localhost:8080";
+            } else {
+              alert("服务器错误");
+            }
+          }
+        })
+      })
+    };
+    
+    //将用户id存到localStorage中
+    function save(tel) {
+      $.ajax({
+        url: "http://localhost:3000/users/getUserId",
+        type: "post",
+        data: {
+          userTel: tel
+        },
+        success: function (result) {
+          if(localStorage) {
+            localStorage.setItem("userId", JSON.stringify(result.data.userId));
+          }
+        }
+      })
     }
+
+    import {mapGetters} from "vuex"
+    export default {
+      name: "LoginList",
+      computed: mapGetters([
+        "isLogin",
+        "userId"
+      ])
+    }
+
 </script>
 
 <style scoped>
