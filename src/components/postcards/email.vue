65a5ff8eb1a0978306c228d713ffc8f6e4e5d85f
Gaoyue <2602121448@qq.com>
2018-10-22 21:46:07 +0800

    完成向接收方发送接收者基本信息的邮件

---
+++ src/components/postcards/email.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      成功
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "email",
+      created(){
+        this.$ajax({
+          method:'get',
+          url:'http://localhost:3000/send/sendEmail'
+        }).then((res)=>{
+       console.log("成功")
+        })
+      }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
